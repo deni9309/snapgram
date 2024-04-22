9b57c6f21643714a18f2233f93dfeeda403ff3a3
Denitsa Slavkova <deni9309@gmail.com>
2024-04-22 22:13:06 +0300

    Integrate react-router-dom; Create main app routes and layouts; Create basic app folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
 import './globals.css';
+
+import SigninForm from "./_auth/forms/SigninForm";
+import SignupForm from "./_auth/forms/SignupForm";
+import { Home } from "./_root/pages";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
 
 const App = () => {
   return (
-    <div className="text-3xl">App</div>
+    <main className="flex h-screen">
+      <Routes>
+        { /* public routes */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+        </Route>
+
+        {/* private routes */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
   );
 };
 
