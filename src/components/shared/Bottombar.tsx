89d7fb4e58240b9dc33ce45bbfbde5e65c6469eb
Denitsa Slavkova <deni9309@gmail.com>
2024-04-25 17:19:07 +0300

    Display bottombar on mobile devices

--- src/components/shared/Bottombar.tsx
+++ src/components/shared/Bottombar.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { Link, useLocation } from "react-router-dom";
+
+import { bottombarLinks } from "@/constants";
 
 const Bottombar = () => {
-  return (
-    <div>Bottombar</div>
-  )
-}
+  const { pathname } = useLocation();
 
-export default Bottombar
+  return (
+    <section className="bottom-bar">
+      {bottombarLinks.map((link, index) => {
+        const isActive = link.route === pathname;
+
+        return (
+          <Link key={index} to={link.route} className={`flex-center flex-col gap-1 p-2 transition ${isActive && 'bg-primary-500 rounded-[10px]'}`}>
+            <img src={link.imgUrl} alt={link.label} width={16} height={16} className={`${isActive && 'invert-white'}`} />
+            <p className="tiny-medium text-light-2">{link.label}</p>
+          </Link>
+        );
+      })}
+    </section>
+  );
+};
+
+export default Bottombar;
