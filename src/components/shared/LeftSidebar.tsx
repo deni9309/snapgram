eff3e3d4b4c3804c393d5a9a04ab8ada02fbb379
Denitsa Slavkova <deni9309@gmail.com>
2024-04-25 16:58:01 +0300

    Create the app pages and include them in the routing

---
+++ src/components/shared/LeftSidebar.tsx
@@ -0,0 +1,72 @@
+import { Link, useNavigate, NavLink, useLocation } from "react-router-dom";
+
+import { useUserContext } from "../../../context/AuthContext";
+import { useSignOutAccount } from "@/lib/react-query/queriesAndMutations";
+import { useEffect } from "react";
+import { sidebarLinks } from "@/constants";
+import { TNavLink } from "@/types";
+import { Button } from "../ui/button";
+
+const LeftSidebar = () => {
+  const { mutateAsync: signOutAccount, isSuccess } = useSignOutAccount();
+  const { user } = useUserContext();
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    if (isSuccess) navigate(0);
+
+  }, [isSuccess]);
+
+  return (
+    <nav className="leftsidebar">
+      <div className="flex flex-col gap-11 ">
+        <Link to="/" className="flex items-center gap-3 max-w-[270px]">
+          <img src="/assets/images/logo.svg" alt="logo" width={170} height={36} />
+        </Link>
+
+        <Link to={`/profile/${user.id}`} className="flex items-center gap-3">
+          <img
+            src={user.imageUrl || "/assets/icons/profile-placeholder.svg"}
+            alt="profile"
+            className="w-12 h-12 rounded-full"
+          />
+          <div className="flex flex-col">
+            <p className="body-bold flex-wrap">{user.name}</p>
+            <p className="small-regular text-light-3">@{user.username}</p>
+          </div>
+        </Link>
+
+        <ul className="flex flex-col gap-6">
+          {sidebarLinks.map((link: TNavLink, index) => {
+            const isActive = link.route === pathname;
+
+            return (
+              <li key={index} className={`leftsidebar-link group ${isActive && 'bg-primary-500'}`}>
+                <NavLink to={link.route} className="flex items-center gap-4 p-4">
+                  <img
+                    src={link.imgUrl}
+                    alt={link.label}
+                    className={`group-hover:invert-white ${isActive && 'invert-white'}`}
+                  />
+                  {link.label}
+                </NavLink>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <Button
+        variant="ghost"
+        className="shad-button_ghost"
+        onClick={() => signOutAccount}
+      >
+        <img src="/assets/icons/logout.svg" alt="logout" width={24} height={24} />
+        <p className="small-medium lg:base-medium">Logout</p>
+      </Button>
+    </nav>
+  );
+};
+
+export default LeftSidebar;
---
+++ src/constants/index.ts
@@ -0,0 +1,50 @@
+export const sidebarLinks = [
+  {
+    label: 'Home',
+    route: '/',
+    imgUrl: '/assets/icons/home.svg',
+  },
+  {
+    label: 'Explore',
+    route: '/explore',
+    imgUrl: '/assets/icons/wallpaper.svg',
+  },
+  {
+    label: 'People',
+    route: '/all-users',
+    imgUrl: '/assets/icons/people.svg',
+  },
+  {
+    label: 'Saved',
+    route: '/saved',
+    imgUrl: '/assets/icons/bookmark.svg',
+  },
+  {
+    label: 'Create Post',
+    route: '/create-post',
+    imgUrl: '/assets/icons/gallery-add.svg',
+  },
+];
+
+export const bottombarLinks = [
+  {
+    label: 'Home',
+    route: '/',
+    imgUrl: '/assets/icons/home.svg',
+  },
+  {
+    label: 'Explore',
+    route: '/explore',
+    imgUrl: '/assets/icons/wallpaper.svg',
+  },
+  {
+    label: 'Saved',
+    route: '/saved',
+    imgUrl: '/assets/icons/bookmark.svg',
+  },
+  {
+    label: 'Create Post',
+    route: '/create-post',
+    imgUrl: '/assets/icons/gallery-add.svg',
+  },
+];
